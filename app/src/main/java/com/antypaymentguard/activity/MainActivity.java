d28bbd373095a1ad84dcfd4024a3defac60aa68e
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-04-16 21:51:25 +0200

    Added add bank account activity

--- app/src/main/java/com/antypaymentguard/activity/MainActivity.java
+++ app/src/main/java/com/antypaymentguard/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.antypaymentguard.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, AddBankAccount.class));
             }
         });
     }
