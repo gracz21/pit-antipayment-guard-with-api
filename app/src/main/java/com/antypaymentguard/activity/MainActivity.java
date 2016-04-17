c40defddd7459d646cc8b62049ee0fbb6d8f814c
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-04-17 16:44:23 +0200

    Added classes and resource files for each tab in add bank account activity

--- app/src/main/java/com/antypaymentguard/activity/MainActivity.java
+++ app/src/main/java/com/antypaymentguard/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.antypaymentguard.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.antypaymentguard.R;
+import com.antypaymentguard.activity.addBankAccount.AddBankAccount;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/antypaymentguard/activity/addBankAccount/TabAuthenticate.java
@@ -0,0 +1,18 @@
+package com.antypaymentguard.activity.addBankAccount;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.antypaymentguard.R;
+/**
+ * @author Kamil Walkowiak
+ */
+public class TabAuthenticate extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_authenticate_add_bank_account, container, false);
+    }
+}
