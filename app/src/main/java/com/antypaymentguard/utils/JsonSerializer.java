2177b12251e2da6b6ab884f447429eb238caf7f9
Maciej Kozłowski <mac229@interia.eu>
2016-05-09 03:04:35 +0200

    Json serializer

---
+++ app/src/main/java/com/antypaymentguard/utils/JsonSerializer.java
@@ -0,0 +1,47 @@
+package com.antypaymentguard.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+
+public class JsonSerializer<T> {
+
+    public String serialize(T data) {
+        final Gson gson = new GsonBuilder().create();
+        return gson.toJson(data);
+    }
+
+    public T deserialize(String data, Type clazzType) {
+        final Gson gson = new GsonBuilder().create();
+        return gson.fromJson(data, clazzType);
+    }
+
+    public T deserialize(Reader data, Type clazzType) {
+        final Gson gson = new GsonBuilder().create();
+        return gson.fromJson(data, clazzType);
+    }
+
+    public T deserialize(Reader data, Class<T> clazz) {
+        final Gson gson = new GsonBuilder().create();
+        return gson.fromJson(data, clazz);
+    }
+
+    public T deserialize(String data, Type clazzType, String object) throws IOException {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(data);
+
+        final Gson gson = new GsonBuilder().create();
+
+        if (jsonElement.isJsonObject()) {
+            return gson.fromJson(jsonElement.getAsJsonObject().get(object), clazzType);
+
+        }
+
+        return null;
+    }
+}
