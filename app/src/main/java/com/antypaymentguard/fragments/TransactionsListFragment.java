2b9f11fa51b23e3f84bfc6882f1ab23a5e88d7e9
Kamil Walkowiak <gracz21@users.noreply.github.com>
2016-05-21 16:11:36 +0200

    Added layout for BankAccountDetailsFragment

--- app/src/main/java/com/antypaymentguard/fragments/TransactionsListFragment.java
+++ app/src/main/java/com/antypaymentguard/fragments/TransactionsListFragment.java
@@ -1,9 +1,20 @@
 package com.antypaymentguard.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.antypaymentguard.R;
 
 /**
  * @author Kamil Walkowiak
  */
 public class TransactionsListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bank_account_details, container, false);
+    }
 }
