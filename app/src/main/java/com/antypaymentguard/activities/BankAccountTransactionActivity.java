c390c2bb9f114f06d20401ac11cbba3ccf079249
Kamil Walkowiak <gracz21@users.noreply.github.com>
2016-06-05 22:55:39 +0200

    Added conditional hide for transaction optional fields

--- app/src/main/java/com/antypaymentguard/activities/BankAccountTransactionActivity.java
+++ app/src/main/java/com/antypaymentguard/activities/BankAccountTransactionActivity.java
@@ -3,6 +3,7 @@ package com.antypaymentguard.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import com.antypaymentguard.R;
@@ -27,8 +28,36 @@ public class BankAccountTransactionActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.bankAccountTextView)).setText(transaction.getBankAccount().getName());
         ((TextView) findViewById(R.id.amountTextView)).setText(transaction.getAmountWithCurrencyName());
         ((TextView) findViewById(R.id.dateTextView)).setText(dateFormat.format(transaction.getDate()));
-        ((TextView) findViewById(R.id.partyTextView)).setText(transaction.getParty());
-        ((TextView) findViewById(R.id.partyIbanTextView)).setText(transaction.getPartyIban());
+
+        TextView partyTextView = ((TextView) findViewById(R.id.partyTextView));
+        String party = transaction.getParty();
+        if(party != null && party.length() > 0) {
+            partyTextView.setText(party);
+        } else {
+            partyTextView.setVisibility(View.GONE);
+            findViewById(R.id.partyLabelTextView).setVisibility(View.GONE);
+            findViewById(R.id.partyIconImageView).setVisibility(View.GONE);
+        }
+
+        TextView partyIbanTextView = ((TextView) findViewById(R.id.partyIbanTextView));
+        String partyIban = transaction.getPartyIban();
+        if(partyIban != null && partyIban.length() > 0) {
+           partyIbanTextView.setText(partyIban);
+        } else {
+            partyIbanTextView.setVisibility(View.GONE);
+            findViewById(R.id.partyIbanLabelTextView).setVisibility(View.GONE);
+            findViewById(R.id.partyIbanIconImageView).setVisibility(View.GONE);
+        }
+
+        TextView kindTextView = ((TextView) findViewById(R.id.kindTextView));
+        String kind = transaction.getKind();
+        if(kind != null && kind.length() > 0) {
+            kindTextView.setText(kind);
+        } else {
+            kindTextView.setVisibility(View.GONE);
+            findViewById(R.id.kindLabelTextView).setVisibility(View.GONE);
+            findViewById(R.id.kindIconImageView).setVisibility(View.GONE);
+        }
         ((TextView) findViewById(R.id.kindTextView)).setText(transaction.getKind());
     }
 
